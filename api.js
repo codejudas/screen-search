42e46614a632b5d7708924ce09f78c6035f987fa
efossier <evan.fossier@gmail.com>
2018-04-28 15:07:25 -0700

    Boilerplate for the api endpoints

--- api.js
+++ api.js
@@ -1,10 +1,26 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get('/api/hello', (req, res) => {
+app.use(bodyParser.json({strict: true}));
+
+/*
+ * Add a new movie to the index
+ */
+app.post('/api/1/movies', (req, res) => {
+  console.log(`Adding movie ${req.body.title}`);
+  // TODO: check for duplicates?
   res.send({"Hello": "World"});
+});
+
+/*
+ * Remove a movie from the index
+ */
+app.delete('/api/1/movies', (req, res) => {
+  console.log(`Removing movie ${req.body.id}`);
+  res.send({"Goodbye": "World"});
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));

ba94e4927ba1093dc4c805d4bcc08185ffcec0e4
efossier <evan.fossier@gmail.com>
2018-04-29 13:18:41 -0700

    WIP Search movies page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './style/app.css';
 import searchIcon from './img/search-icon.svg';
+import movieIcon from './img/film.svg';
 import AlgoliaClient from 'algoliasearch';
 
 class App extends Component {
@@ -35,7 +36,7 @@ class App extends Component {
   render() {
     return (
     <div className="container">
-      <SearchBox onQueryChanged={this.updateSearchResults.bind(this)} />
+      <SearchBox onQueryChanged={this.updateSearchResults.bind(this)} placeholder="Start typing to discover movies..." />
       <SearchResults results={this.state.results} query={this.state.query} />
     </div>
     );
@@ -67,7 +68,7 @@ class SearchBox extends Component {
     return (
       <div className="search-box">
         <img src={searchIcon} alt='Search for a movie!'/>
-        <input className="search-input" type="text" placeholder="TBD" onChange={evt => this.onInputChange(evt)}/>
+        <input className="search-input" type="text" placeholder={this.props.placeholder} onChange={evt => this.onInputChange(evt)}/>
       </div>
     );
   }
@@ -78,13 +79,76 @@ class SearchResults extends Component {
     super(props);
   }
 
+  hasQuery() {
+    return this.props.query && this.props.query !== '';
+  }
+
+  getSearchHeader() {
+    if (!this.hasQuery()) {
+      return '';
+    }
+    return `Found ${this.props.results.nbHits} matches`;
+
+  }
+
+  render() {
+    let headerClasses = 'search-header';
+    if (!this.hasQuery()) {
+      headerClasses += ' hidden';
+    }
+
+    // let results = [<SearchEntryPlaceholder key='1' />];
+    let results = [];
+    if (this.hasQuery()) {
+      this.props.results.hits.forEach((movie, index, arr) => {
+        results.push(<SearchEntry movieData={movie} idx={index} key={`${index}-entry`} />);
+        if (index != (arr.length - 1)) {
+          results.push(<SearchDivider key={`${index}-divider`} />);
+        }
+        
+      });
+    }
+
+    return (
+      <div className="search-content">
+        <div className={headerClasses}>
+          {/* <i className="fas fa-film movie-icon"></i><span>Found some stuff</span> */}
+          <i className="fas fa-video movie-icon"></i><span>{this.getSearchHeader()}</span>
+        </div>
+        <div className="search-results"> 
+        {results}
+        </div>
+      </div>
+    );
+  }
+}
+
+class SearchEntry extends Component {
   render() {
     return (
-      <ol className="search-results"> 
-        { this.props.results.hits.map(movie => 
-            <li key={movie.title}>{movie.title}</li>
-          )}
-      </ol>
+      <div className="search-entry">
+        <span className="index">{this.props.idx + 1}</span>
+        <img src={this.props.movieData.image} className="movie-image" />
+        <span className="info">{this.props.movieData.title}</span>
+      </div>
+    );
+  }
+}
+
+class SearchDivider extends Component {
+  render() {
+    return (
+      <div className="search-divider" />
+    );
+  }
+}
+
+class SearchEntryPlaceholder extends SearchEntry {
+  render() {
+    return (
+      <div className="search-entry">
+        Nothing to show. Start typing to discover movies...
+      </div>
     );
   }
 }

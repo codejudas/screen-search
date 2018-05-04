6f1d74207c24a808ea16e90538dd53b057d4e39d
efossier <evan.fossier@gmail.com>
2018-05-03 22:04:25 -0700

    Trap ctrl c

--- deploy-heroku.sh
+++ deploy-heroku.sh
@@ -6,6 +6,20 @@ set -e
 # Echo commands
 set -x
 
+# This function is called when Ctrl-C is sent
+function trap_ctrlc ()
+{
+    # perform cleanup here
+    echo "Ctrl-C caught... returning to master"
+ 
+    git checkout master
+    exit 2
+}
+
+# initialise trap to call trap_ctrlc function
+# when signal 2 (SIGINT) is received
+trap "trap_ctrlc" 2
+
 git checkout build
 git reset --hard master
 npm run build

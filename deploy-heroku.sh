18f11a24d29e92ae7a8bd0493dcf0ebea2ee057d
efossier <evan.fossier@gmail.com>
2018-05-03 21:39:48 -0700

    Setup to deploy to heroku

---
+++ deploy-heroku.sh
@@ -0,0 +1,13 @@
+set +x
+
+git checkout build
+git reset --hard master
+npm run build
+git add --force client/build
+git commit -am "Built client side code"
+git push -f heroku build:master
+echo "================"
+echo "====DEPLOYED===="
+echo "================"
+sleep 1
+heroku logs
